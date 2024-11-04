a239443725128808d92c639469340b808810575f
Manibharathi <58763956+mani195@users.noreply.github.com>
2024-11-04 09:27:21 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,32 +1,73 @@
 #pragma once
+#include <memory>
+#include <map>
+#include "BreachType.h"
 
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
+class CoolingStrategy
+{
+    public:
+    virtual ~CoolingStrategy()= default;
+    virtual BreachType inferBreach(double value) const = 0;
+};
 
-typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
-} BreachType;
+class PassiveCooling : public CoolingStrategy
+{
+    public:
+    BreachType inferBreach(double value) const override;
+};
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+class HiActiveCooling : public CoolingStrategy
+{
+    public:
+    BreachType inferBreach(double value) const override;
+};
 
-typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
-} AlertTarget;
+class MedActiveCooling : public CoolingStrategy
+{
+    public:
+    BreachType inferBreach(double value) const override;
+};
 
-typedef struct {
-  CoolingType coolingType;
-  char brand[48];
-} BatteryCharacter;
+class CoolingContext 
+{
+  private:
+  std::unique_ptr<CoolingStrategy> strategy;
+  
+  public:
+  CoolingContext(std::unique_ptr<CoolingStrategy> strategy);
+  BreachType inferBreach(double value) const;  
+};
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+class AlertStrategy 
+{
+public:
+    virtual ~AlertStrategy() = default;
+    virtual void report(const BreachType breachType) = 0;
+};
 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+class ControllerAlert : public AlertStrategy 
+{
+public:
+    void report(const BreachType breachType) override;
+};
+
+class EmailAlert : public AlertStrategy 
+{
+public:
+    void report(const BreachType breachType) override;
+private:
+    const std::map<const BreachType, std::string> breachMessages =
+    {
+        {BreachType::TOO_LOW, "the temperature is too low"},
+        {BreachType::TOO_HIGH, "the temperature is too high"},
+    };
+};
+
+class Alerter
+{
+  private:
+      std::unique_ptr<AlertStrategy> strategy;
+  public:
+      Alerter(std::unique_ptr<AlertStrategy> strategy);
+      void report(const BreachType breachType);
+};
